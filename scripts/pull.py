699fee612855517a212c470237254e3b87536cad
Timo Klok <mail@timoklok.nl>
2014-05-19 16:05:56 +0200

    the year is now CL argument

--- scripts/pull.py
+++ scripts/pull.py
@@ -1,4 +1,4 @@
-import urllib2, urllib, json, sys, html5lib, os, unicodedata, string
+import urllib2, urllib, json, sys, html5lib, os, unicodedata, string, time, argparse
 from pprint import pprint
 from xml.etree import ElementTree as ET
 
@@ -16,17 +16,25 @@ def strip_accents(s):
    return ''.join(c for c in unicodedata.normalize('NFD', s)
                   if unicodedata.category(c) != 'Mn')
 
+parser = argparse.ArgumentParser(description='Which year')
+parser.add_argument('year', metavar='N', type=int, nargs='+',
+                   help='duh.... the year...')
+
 # def remove_accents(data):
 #     return ''.join(x for x in unicodedata.normalize('NFKD', data) if x in string.ascii_letters).lower()
+# |?Year
+
+yearrequested = str(parser.parse_args().year[0])
+print yearrequested
 
 API = "http://pzwart3.wdka.hro.nl/mediawiki/api.php"
 qvars = {
     'action': 'ask',
     'query':'''{{#ask:[[Category:Grad project]]
+[[Year::%s]]
 |?#
 |?Student
 |?Project
-|?Year
 |?Featured image
 |?Project
 |?Bio
@@ -34,7 +42,7 @@ qvars = {
 |?Project URL
 |format=jsonfm
 |link=all
-}}''',
+}}''' % yearrequested,
     'format': 'json'
 }
 qurl = API + "?" + urllib.urlencode(qvars)
@@ -51,8 +59,10 @@ for title in data['query']['results']:
     d = {}
     metadata[title] = d
     for key, value in data['query']['results'][title]['printouts'].items():
-        # print "\t", key, unwrap(value)
-        d[key] = unwrap(value)
+        if key == 'Year':
+            d[key] = time.strftime('%Y', unwrap(value) )
+        else:
+            d[key] = unwrap(value)
 
 qvars = {
     'action': 'query',
@@ -73,7 +83,6 @@ for item in data['query']['categorymembers']:
     }
     qurl = API + "?" + urllib.urlencode(qvars)
     data = json.load(urllib2.urlopen(qurl))
-    # pprint(data)
     src = data['parse']['text']['*']
     tree = html5lib.parseFragment(src, namespaceHTMLElements=False)
     kill = []
@@ -85,12 +94,12 @@ for item in data['query']['categorymembers']:
 
     md = metadata[title]
     md['layout'] = 'post'
+    md['year'] = yearrequested
     md['permalink'] = strip_accents(md.get("Student").lower().replace(" ", "-"))
-
     fpath = os.path.join("_posts", "2014-05-12-"+md['permalink']+".html")
     with open(fpath, "w") as f:
         out = u"---\n"
-        out += 'layout: post'
+        #out += 'layout: post'
         out += u"\n"
         keys = md.keys()
         keys.sort()
